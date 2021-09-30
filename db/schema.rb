d125b4b7a8b79f85af079f5b13a16b73babdc3d4
Edward Bent <bentorama@gmail.com>
2021-09-30 19:13:59 +0100

    Created Favourite model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_30_180139) do
+ActiveRecord::Schema.define(version: 2021_09_30_180907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 2021_09_30_180139) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "favourites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "beer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["beer_id"], name: "index_favourites_on_beer_id"
+    t.index ["user_id"], name: "index_favourites_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -47,4 +56,6 @@ ActiveRecord::Schema.define(version: 2021_09_30_180139) do
   end
 
   add_foreign_key "beers", "breweries"
+  add_foreign_key "favourites", "beers"
+  add_foreign_key "favourites", "users"
 end
