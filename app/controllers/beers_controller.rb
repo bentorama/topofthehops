b0c3c21dbb200e218eac81923af830af39aa55c8
Edward Bent <bentorama@gmail.com>
2021-10-02 16:08:29 +0100

    Logic for searching db then callin api working

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,6 +1,7 @@
 class BeersController < ApplicationController
   require 'open-uri'
   require 'json'
+  require_relative 'breweries_controller'
 
   def index
     if params["search"]["beer_1"].present? && params["search"]["beer_1"] != ""
@@ -8,13 +9,8 @@ class BeersController < ApplicationController
       @ratings = []
       params["search"].each_value do |value|
         result = Beer.global_search(value).first
-        if result
-          @beers << result
-        else
-          call_untappd(value)
-        end
+        result ? @beers << result : call_untappd(value)
       end
-      call_ratings
     else
       @beer = "Nothing searched"
     end
@@ -28,22 +24,24 @@ class BeersController < ApplicationController
     url = "https://api.untappd.com/v4/search/beer?q=#{search}&client_id=#{untappd_id}&client_secret=#{untappd_secret}"
     beer_serialized = URI.parse(url).open.read
     result = JSON.parse(beer_serialized)["response"]["beers"]["items"].first
+    brewery = brewery_in_db(result)
+    rating = get_rating(result)
+    beer = add_beer(result["beer"], rating, brewery)
+    @beers << beer
   end
 
-  def call_ratings
+  def get_rating(beer)
     untappd_id = ENV['UNTAPPD_ID']
     untappd_secret = ENV['CLIENT_SECRET']
-    @beers.each do |beer|
-      bid = beer["beer"]["bid"]
-      url = "https://api.untappd.com/v4/beer/info/#{bid}?&client_id=#{untappd_id}&client_secret=#{untappd_secret}"
-      beer_serialized = URI.parse(url).open.read
-      @ratings << JSON.parse(beer_serialized)["response"]["beer"]["rating_score"]
-    end
+    bid = beer["beer"]["bid"]
+    url = "https://api.untappd.com/v4/beer/info/#{bid}?&client_id=#{untappd_id}&client_secret=#{untappd_secret}"
+    beer_serialized = URI.parse(url).open.read
+    JSON.parse(beer_serialized)["response"]["beer"]["rating_score"]
   end
 
   def brewery_in_db(beer)
     untappd_id = beer["brewery"]["brewery_id"]
-    result = Brewery.search_by_untappd_id(untappd_id)
+    result = Brewery.search_by_untappd_id(untappd_id).first
     result ||= add_brewery(beer["brewery"])
     result
   end
@@ -56,5 +54,17 @@ class BeersController < ApplicationController
     new_brewery.country = brewery["country_name"]
     new_brewery.save!
     new_brewery
+  end
+
+  def add_beer(beer, rating, brewery)
+    new_beer = Beer.new
+    new_beer.name = beer["beer_name"]
+    new_beer.style = beer["beer_style"]
+    new_beer.description = beer["beer_description"]
+    new_beer.untappd_rating = rating
+    new_beer.untappd_id = beer["bid"]
+    new_beer.brewery = brewery
+    new_beer.save!
+    new_beer
   end
 end
